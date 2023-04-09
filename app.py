3d5ad6c1792d2f7f0d5e2cb5745d56cb90a5aa96
@Egsagon <egsagon12@gmail.com>
2023-04-09 19:14:49 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,98 @@
+import flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+import nekosama
+from nekosama.consts import root
+
+
+client = nekosama.Client()
+
+app = flask.Flask(__name__, static_folder = 'client/')
+lim = Limiter(get_remote_address,
+              app = app,
+              storage_uri="memory://")
+
+@app.route('/')
+def home():
+    # Handle home redirect
+    
+    return flask.send_file('client/index.html', mimetype = 'text/html')
+
+@app.route('/eps')
+# @lim.limit('30/hour')
+def handle_episodes():
+    # Handle fetching animes
+    
+    rel = flask.request.args.get('rel')
+    
+    # Error protection
+    if rel is None: return flask.jsonify({'error': 'invalid'})
+    
+    # Fetch the anime
+    url = root + '/anime/info/' + rel
+    try: anime = client.get_anime(url)
+    except Exception as err:
+        return flask.jsonify({'error': str(err)})
+    
+    try:
+        episodes = anime.episodes
+    
+    except ConnectionError:
+        return flask.jsonify({'error': 'invalid url'})
+    
+    # Get the episodes
+    data = [{
+        'url': ep.url,
+        'index': ep.index,
+        'time': ep.time,
+        'image': ep.get_image_url(),
+        'name': ep.name
+    } for ep in episodes]
+    
+    return flask.jsonify(data)
+
+
+@app.route('/get')
+# @lim.limit('30/hour')
+def handle_download():
+    # Handle downloading animes
+    
+    url = flask.request.args.get('url')
+    quality = flask.request.args.get('q') or 'best'
+    
+    # Error protection
+    if url is None: return flask.jsonify({'error': 'invalid'})
+    
+    # Fetch the anime
+    try: source = nekosama.Episode(url)
+    except Exception as err:
+        print('error in /get:', err)
+        return 'error:' + str(err)
+    
+    def stream():
+        # Stream the source output
+        
+        chunks = source.get_fragments(quality = quality)
+        chunks = [c for c in chunks.split() if not c[0] == '#']
+        
+        for url in chunks:
+            print(url)
+            yield source.get(url).content
+    
+    filename = source.name + '.mp4'
+    
+    print('streaming source to frontend')
+    
+    return flask.Response(
+        flask.stream_with_context(stream()),
+        headers = {
+            'Content-Disposition': f'attachment; filename={filename}',
+            'Content-Type': 'application/octet-stream'
+        }
+    )
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+# EOF
---
+++ client/app.js
@@ -0,0 +1,159 @@
+url_entry = document.querySelector('#url')
+download = document.querySelector('#download')
+idownload = document.querySelector('#download img')
+strart_dl = document.querySelector('#confirm')
+select_all_btn = document.querySelector('#all')
+
+popup = document.querySelector('.info')
+form = document.querySelector('.search')
+results = document.querySelector('.results')
+popup_dls = document.querySelector('.info ul')
+
+background = document.querySelector('.bg')
+
+selected = `
+<svg width="20" height="20">
+  <circle cx="10" cy="10" r="10" fill="rgba(0, 0, 0, 0.5)" />
+  <path d="M3 9 L8 16 L16 3" stroke="green" stroke-width="3" fill="none" />
+</svg>
+`
+
+SELECTED = []
+
+function init_dl() {
+    // Initialise the download of all selected files
+
+    for (let ep_url of SELECTED) {
+        window.open(window.location.href + 'get?url=' + ep_url, '_blank').focus()
+    }
+}
+
+function select_all() {
+    // Select all elements
+
+    results.querySelectorAll('div').forEach(el => {select(el)})
+}
+
+function cancel_dl() {
+    // Remove animations
+
+    popup.classList.add('hidden')
+
+    background.classList.remove('animate')
+    background.classList.add('deanimate')
+
+    setTimeout(() => {
+        background.classList.remove('deanimate')
+        document.body.style.overflowX = 'unset'
+        document.body.style.overflowY = 'unset'
+    }, 1050)
+}
+
+function confirm_dl() {
+    // Confirmation popup
+
+    document.body.style.overflowX = 'hidden'
+    document.body.style.overflowY = 'hidden'
+
+    // Animate
+    background.classList.add('animate')
+    popup.classList.remove('hidden')
+
+    setTimeout(() => {
+        popup.style.opacity = 1
+    }, 800)
+
+    nodes = results.querySelectorAll('div')
+
+    SELECTED = []
+
+    popup_dls.querySelectorAll('li').forEach(el => {
+        el.remove()
+    })
+
+    for (let node of nodes) {
+        if (node.dataset.selected) {
+            SELECTED.push(node.dataset.url)
+
+            li = document.createElement('li')
+            li.innerHTML = node.dataset.name
+            popup_dls.appendChild(li)
+        }
+    }
+
+    console.log('confirming', SELECTED)
+
+    if (!SELECTED.length) {
+        li = document.createElement('li')
+        li.innerHTML = 'You haven\'t selected anything yet.'
+        popup_dls.appendChild(li)
+    }
+}
+
+function select(el) {
+    // Triggered when one episode is clicked
+
+    svg = el.querySelector('svg').classList
+
+    svg.toggle('selected')
+    el.removeAttribute('data-selected')
+
+    if (svg.contains('selected')) {
+        el.setAttribute('data-selected', true)
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    // Called on form submit
+
+    e.preventDefault()
+
+    url = url_entry.value
+    rel = url.split('/').slice(-1)[0]
+
+    // Send the request
+    xhr = new XMLHttpRequest()
+    xhr.open('GET', '/eps?rel=' + rel)
+    xhr.setRequestHeader('Content-Type', 'text/html')
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // On receive response
+
+            try {
+                response = JSON.parse(xhr.response)
+            
+                // Load elements to document
+                for (let episode of response) {
+                    
+                    el = document.createElement('div')
+                    el.setAttribute('data-url', episode.url)
+                    el.setAttribute('data-name', episode.name)
+
+                    el.innerHTML = `<span class='ep-idx'>Episode ${episode.index}</span><span class='ep-dur'>${episode.time} min</span>`
+                    el.innerHTML += selected // Add the SVG icon
+
+                    el.addEventListener('click', select.bind(this, el))
+
+                    // Set the background image
+                    el.style.backgroundImage = `url(${episode.image})`
+
+                    results.appendChild(el)
+                }
+            
+            // Error protection
+            } catch (err) {alert('Invalid URL'); throw err}
+
+            // Move the search bar
+            // TODO
+
+            // Show download and select buttons
+            select_all_btn.classList.remove('hidden')
+            download.classList.remove('hidden')
+            idownload.classList.remove('hidden')
+        }
+    }
+
+    xhr.send()
+})
+
